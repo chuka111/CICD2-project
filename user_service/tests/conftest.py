fbd66e0b6b02312e7018f9f7dacb03e989b3b04e
chuka111 <118300161+chuka111@users.noreply.github.com>
2026-01-09 20:35:29 +0000

    added notification and login

---
+++ user_service/tests/conftest.py
@@ -0,0 +1,11 @@
+#user_service/tests/conftest.py
+import sys
+from pathlib import Path
+import pytest
+from fastapi.testclient import TestClient
+sys.path.append(str(Path(__file__).resolve().parents[1]))
+from app.main import app
+
+@pytest.fixture
+def client():
+    return TestClient(app)
